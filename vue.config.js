b08b7eb4205486bd56592714d33afbe972634a07
Joan-Thirimbu <joanwangare3@gmail.com>
2025-02-10 17:21:52 +0300

    favicon & title setup.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        title: 'Joan Thirimbu',
+      }),
+    ],
+  },
+});
