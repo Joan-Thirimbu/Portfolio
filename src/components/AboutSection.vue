0c4f341f1120fe92f9d6740a11f7006b525e73b7
Joan-Thirimbu <joanwangare3@gmail.com>
2025-02-11 22:22:09 +0300

    AboutSection initial code & assets update

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,14 +1,173 @@
 <template>
     <section id="about">
-        <h1>About Section</h1>
+        <div class="about-container">
+            <h1 class="text-center">About</h1>
+            <div class="info">
+                <img src="../assets/profile.webp"/>
+                <div class="info-text" v-html="about"></div>
+            </div>
+            <div class="role-cards">
+                <div class="card" v-for="role in roles" :key="role.id">
+                    <div class="card-title">
+                        <img :src="role.svg">
+                        <div class="title-text">
+                            <span class="heading">{{ role.title }}</span>
+                            <span class="sub-title">{{ role.heading }}</span>
+                        </div>
+                    </div>
+                    <div class="role-desc">
+                        <span class="element">&lt;/h4&gt;</span>
+                        <p>{{ role.info }}</p>
+                        <span class="element">&lt;/h4&gt;</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
 <script>
+import { roles, aboutInfo } from '@/data.js';
 
 export default{
     name: "AboutSection",
+    data(){
+        return{
+            roles,
+            about: aboutInfo,
+        }
+    },
     components:{},
 }
-
 </script>
+
+<style scoped>
+.about-container{
+    margin-inline: clamp(2em, 20em, 12vw);
+    padding-bottom: 90px;
+}
+h1{
+    font-size: 56px;
+    font-weight: 700;
+    padding-top: 70px;
+}
+.info{
+    padding-top: 50px;
+    display: grid;
+    gap: 2.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(336px, 1fr));
+    align-items: center;
+}
+.info img{
+    width: 100%;
+    max-width: 492px;
+    height: auto;
+}
+::v-deep .info p{
+    text-align: justify;
+}
+::v-deep .key{
+    color: #1E1E1E;
+    font-family: "Mynerve";
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+}
+.role-cards{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+    padding-top: 100px;
+    margin: 0;
+}
+.card{
+    display: flex;
+    padding: 40px 30px;
+    flex-direction: column;
+    gap: 22px;
+    border: 1px solid #F8F7F5;
+    height: 100%;
+}
+.card-title{
+    display: flex;
+    align-items: center;
+    gap: 24px;
+}
+.card-title img{
+    width: auto;
+    max-width: 40px;
+    min-width: 30px;
+    height: auto;
+}
+.title-text{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+.title-text span{
+    font-size: 26px;
+    font-weight: 600;
+    line-height: 32px;
+    text-decoration: none;
+    padding-inline: 5px;
+}
+.heading{
+    position: relative;
+}
+.heading::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    left: 0;
+    width: 100%;
+    height: 8px;
+    background-color: #d397b1;
+    transform: translateY(-7px);
+    z-index: -1;
+}
+.element{
+    font-size: 12px;
+    color: #1E1E1E;
+}
+.role-desc p{
+    text-align: unset;
+    position: relative;
+    padding-left: 40px;
+    font-size: 16px;
+    font-weight: 600;
+    letter-spacing: 0.64px;
+}
+.role-desc p::after{
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 1px;
+    height: 100%;
+    margin-left: 15px;
+    background-color: #1E1E1E;
+}
+@media screen and (max-width: 960px){
+    .info{
+        justify-items: center;
+        grid-template-columns: 1fr;
+    }
+    .info-text{
+        padding-top: 40px;
+    }
+    .role-cards{
+        padding-top: 60px;
+    }
+}
+@media screen and (max-width: 512px){
+    .info-text{
+        padding-top: 20px;
+    }
+    .role-cards{
+        grid-template-columns: 1fr;
+        padding-top: 60px;
+    }
+    .title-text span{
+        font-size: 22px;
+    }
+}
+</style>
---
+++ src/data.js
@@ -0,0 +1,29 @@
+import design from '../src/assets/ui.svg';
+import computer from '../src/assets/computer.svg';
+import code from '../src/assets/code.svg';
+
+export const roles = [
+    {
+        id: '1',
+        title: 'Software',
+        heading: 'Development',
+        info: 'Experienced in OOP, Event-Driven and Component-based development: Python, PhP, Javascript, TypeScript',
+        svg: computer,
+    },
+    {
+        id: '2',
+        title: 'Frontend Dev',
+        heading: 'Vue, Angular',
+        info: 'Over 2 years experience of development in HTML, CSS. JS, PHP, Vue and Angular frameworks.',
+        svg: code,
+    },
+    {
+        id: '3',
+        title: 'UX/UI Design',
+        heading: 'Figma, XD',
+        info: 'Passionate about UI/UX. Over 3 years experience designing with Figma and Adobe XD.',
+        svg: design,
+    },
+];
+
+export const aboutInfo = `<p>As a <span class="key">Frontend Focused Developer</span> and <span class="key">UX/UI Designer</span> based in Kenya, I get to combine my love for technology with my passion for art and design.</p><br><p>I attained my Bachelor’s degree in <span class="key">Software Development</span> from <span class="key">KCA University</span>, and I am currently pursuing the <span class="key">Google UX Design Certificate</span> on <span class="key">Coursera</span>.</p><br><p>I enjoy using my creativity and problem-solving skills to create digital experiences that not only look great but also work seamlessly for the user. When Im not designing or coding, you can usually find me exploring the city or my neighbourhood in search for inspiration while taking in the sceneries. I believe that continuous learning is the key to success, and ï'm always on the look out for fresh challenges to keep me motivated and engaged.</p>`;
