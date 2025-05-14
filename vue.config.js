c194dc843bddd11b517c2c9d5bdc23001012bbce
Joan-Thirimbu <joanwangare3@gmail.com>
2025-05-15 00:59:41 +0300

    favicon fix

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: 'public/index.html',
-        title: 'Joan Thirimbu',
-      }),
-    ],
-  },
-  publicPath: '/Portfolio/'
+  publicPath: '/Portfolio/',
 });
