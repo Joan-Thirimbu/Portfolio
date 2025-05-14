6430294f80e58d6e04aa4788d0baf1b9a4944231
Joan-Thirimbu <joanwangare3@gmail.com>
2025-05-15 01:30:34 +0300

    revert navigateToProject function-for testing

--- src/components/WorkDetails.vue
+++ src/components/WorkDetails.vue
@@ -97,7 +97,7 @@ export default {
             this.project = projects.find(project => project.title == this.title);
         },
         navigateToProject(projectId) {
-            window.location.href = window.location.origin + '/Portfolio/#/work/' + projectId;
+            window.location.href = '/work/' + projectId;
         },
     },
     computed: {
